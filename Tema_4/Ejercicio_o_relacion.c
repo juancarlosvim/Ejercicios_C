30d88563878991d963f972bd1af3e0e79e487c3b
Juan Carlos <juancarlosvillegasmarmol@gmail.com>
2016-10-16 15:30:50 +0200

    Update Ejercicio_o_relacion.c

--- Tema_4/Ejercicio_o_relacion.c
+++ Tema_4/Ejercicio_o_relacion.c
@@ -1,10 +1,10 @@
-//o) Calcular el producto de los N primeros números naturales (sin contar el cero), es decir el
+//o) Calcular el producto de los N primeros nÃºmeros naturales (sin contar el cero), es decir el
 //factorial de N (N!).
 #include <stdio.h>
 
 int main()
 {
-    int i=0 , n =0, producto=1;
+    int i, n =0, producto=1;
 
     printf("Introduce un numero para calcular el producto de los N primeros numeros naturales: ");
     scanf("%d", &n);
