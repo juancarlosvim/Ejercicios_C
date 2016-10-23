28f743684542e283e84e9bdcdddf2a22bbeddb46
Juan Carlos <juancarlosvillegasmarmol@gmail.com>
2016-10-23 22:05:56 +0200

    Update Ant_4_31_V1.c

--- Tema_4/Ant_4_31_V1.c
+++ Tema_4/Ant_4_31_V1.c
@@ -1,29 +1,29 @@
-+#include <stdio.h>
-  +
-  +int resdiv(int, int);
-  +
-  +int main(){
-  +    
-  +    int numero1, numero2;
-  +    
-  +    printf("Introduzca el dividendo y el divisor ");
-  +    scanf("%d%d", &numero1, &numero2);
-  +    resdiv(numero1, numero2);
-  +    return 0;
-  +}
-  +
-  +int resdiv(int a, int b){
-  +    
-  +    int resultado;
-  +    
-  +    if(a>=b){
-  +
-  +        a-=b;
-  +        printf("%d y %d ",a ,b);
-  +        resultado = resdiv(a, b);
-  +
-  +
-  +    }else{
-  +        return resultado;
-  +    }
-  +}
+#include <stdio.h>
+
+int resdiv(int, int);
+
+int main(){
+    
+    int numero1, numero2;
+    
+    printf("Introduzca el dividendo y el divisor ");
+    scanf("%d%d", &numero1, &numero2);
+    resdiv(numero1, numero2);
+    return 0;
+}
+
+int resdiv(int a, int b){
+    
+    int resultado;
+    
+    if(a>=b){
+
+        a-=b;
+        printf("%d y %d ",a ,b);
+        resultado = resdiv(a, b);
+
+
+    }else{
+        return resultado;
+    }
+}
