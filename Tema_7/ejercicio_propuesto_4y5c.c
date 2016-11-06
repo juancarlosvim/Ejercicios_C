012696b3c8316dbe97a3f2d016e04e5c895e4cc0
Juan Carlos <juancarlosvillegasmarmol@gmail.com>
2016-11-06 19:57:22 +0100

    Add files via upload

---
+++ Tema_7/ejercicio_propuesto_4y5c.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int matriz[5][10]; // tamaño de la matriz
+    int i; //filas
+    int j; //columnas
+    int minimo =100, maximo =0;
+    int filaMayor, columnaMayor;
+    int filaMenor, columnaMenor;
+
+
+    srand(time(NULL));
+
+    for(i=0;i<5;i++) //imprimir matriz
+    {
+        for(j=0;j<10;j++)
+        {
+            matriz[i][j] = rand()%90+1;
+            printf("%4d", matriz[i][j]);
+
+
+
+
+
+	    if(matriz[i][j] > maximo)
+            {
+                filaMayor = i;
+                columnaMayor = j;
+                maximo = matriz[i][j];
+            }
+
+	    if(matriz[i][j] < minimo)
+            {
+                filaMenor = i;
+                columnaMenor = j;
+                minimo = matriz[i][j];
+            }
+        }
+        printf("\n");
+
+
+
+    }
+
+
+    printf("Mayor %d posicion (%d,%d)\n", maximo, filaMayor, columnaMayor);
+    printf("Menor %d posicion (%d,%d)\n", minimo, filaMenor, columnaMenor);
+
+    printf("\n\n\nMATRIZ TRASPUESTA\n\n\n");
+
+    for(i=0;i<10;i++) //imprimir matriz traspuesta
+    {
+        for(j=0;j<5;j++)
+        {
+            printf("%4d", matriz[j][i]);
+        }
+        printf("\n");
+    }
+return 0;
+}
